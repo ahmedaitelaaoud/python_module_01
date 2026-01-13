4dcef882a2fbbe4137a5a348c7257af6902d1d0d
ahmedaitelaaoud <ahmedaitelaaoud@gmail.com>
2026-01-13 15:13:37 +0100

    Enhance docstrings for Plant class methods to improve clarity and detail

--- ex2/ft_plant_growth.py
+++ ex2/ft_plant_growth.py
@@ -1,14 +1,23 @@
 class Plant:
+    """
+    Represents a plant that can grow over time.
+    """
     def __init__(self, name, height, age):
         self.name = name
         self.height = height
         self.age = age
 
     def grow(self):
+        """
+        Simulates one cycle of growth for the plant.
+
+        Increments the height by 1cm and age by 1 day.
+        """
         self.height += 1
         self.age += 1
 
     def get_info(self):
+        """Prints current status."""
         print(f"{self.name}: {self.height}cm, {self.age} days old")
 
 
